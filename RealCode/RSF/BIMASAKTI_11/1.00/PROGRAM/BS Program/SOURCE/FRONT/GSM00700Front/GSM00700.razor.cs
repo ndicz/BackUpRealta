5aa9fa6a1a4ff99375bbf4fb43ef5f5fb07cf59f
Hendri Praminiarto <90389025+ndicz@users.noreply.github.com>
2023-07-14 17:34:29 +0700

    ssaasdasd

--- RealCode/RSF/BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/FRONT/GSM00700Front/GSM00700.razor.cs
+++ RealCode/RSF/BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/FRONT/GSM00700Front/GSM00700.razor.cs
@@ -15,7 +15,7 @@ using R_BlazorFrontEnd.Helpers;
 using R_CommonFrontBackAPI;
 
 namespace GSM00700Front uhuy
-{
+{sss
     public partial class GSM00700 : R_Page
     {
         private GSM00700ViewModel GSM00700ViewModel = new();
